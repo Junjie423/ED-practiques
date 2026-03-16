33bc79e06cb79fe2a64334814d2630832ab7ebbc
Junjie423 <jzhouzho423@alumnes.ub.edu>
2026-03-16 12:31:08 +0000

    Initial Commit

--- Practica 2/Codi/Exercici3/NodeList.h
+++ Practica 2/Codi/Exercici3/NodeList.h
@@ -7,11 +7,16 @@ using namespace std;
 template <class Element> class NodeList{
 
 public:
-
+    NodeList(); // inicialitza NodeList per defecte
+    NodeList(const Element); // inicialitza amb l'element que rep com a paràmetre
+    NodeList(const NodeList &original); // constructor còpia
+    ~NodeList(); // destructor 
+    Element& accesElement() const;
 
 private:
-
-
+    Element _element;
+    int _previous;
+    int _next;
 };
 
 #endif // NODE_LIST_H
