d39af39fa0258514fdb86329029d22a1f5d77dd5
Junjie423 <jzhouzho423@alumnes.ub.edu>
2026-04-20 11:55:12 +0000

    11:54

--- Practica 3/BinaryTree.h
+++ Practica 3/BinaryTree.h
@@ -1,6 +1,12 @@
 #ifndef BINARYTREE_H
 #define BINARYTREE_H
 
+#include "BinaryTree.h"
+#include "Position.h"
+#include <vector>
+#include <iostream>
+using namespace std;
+
 template <class Key, class Value>
 class BinaryTree {
 public:
@@ -25,4 +31,111 @@ private:
     int _size;
     /* Mètodes auxiliars definiu aquí els que necessiteu */    
 }; 
+
+// Constructors
+template <class Key, class Value> BinaryTree<Key, Value>::BinaryTree(){
+    this->root = nullptr;
+    this->_size = 0;
+}
+
+template <class Key, class Value> BinaryTree<Key, Value>::BinaryTree(const BinaryTree<Key, Value>& orig){
+    this->root = new Postion<Key, Value> (orig->getRoot());
+    this->_size = orig->size();
+}
+
+// Destructor
+template <class Key, class Value> BinaryTree<Key, Value>::~BinaryTree(){
+    // Cal omplir
+}
+
+// Consultors
+template <class Key, class Value> bool BinaryTree<Key, Value>::isEmpty() const{
+    return this->_size == 0;
+}
+
+template <class Key, class Value> Position<Key, Value>* BinaryTree<Key, Value>::getRoot() const{
+    return this->root;
+}
+
+template <class Key, class Value> int BinaryTree<Key, Value>::size() const{
+    return this->_size;
+}
+
+/* Mapa mental
+              02
+        /             \        
+      00              08  
+   /      \        /      \
+  --      --      05      45
+ /  \    /  \    /  \    /  \ 
+--  --  --  --  03  --  40  76 
+*/
+template <class Key, class Value> bool BinaryTree<Key, Value>::contains(const Key& key) const{
+    if(this->root == nullptr){
+        throw out_of_range("No hi ha cap element a l'arbre");
+    }
+    Position<Key, Value>* actual = this->root;
+    bool trobat = false;
+    while(!trobat && !actual != nullptr){
+        if (actual->getKey() > key){
+            actual = actual->left();
+        }
+        else if (actual->getKey() < key){
+            actual = actual->right();
+        }
+        else{
+            trobat = true;
+        }
+    }
+    return trobat;
+}
+
+template <class Key, class Value> Position<Key, Value>* BinaryTree<Key, Value>::search(const Key& key) const{
+
+}
+
+template <class Key, class Value> const vector<Value>& BinaryTree<Key, Value>:: getValues(const Key& key) const{
+
+}
+
+// Modificador
+template <class Key, class Value> Position<Key, Value>* BinaryTree<Key, Value>::insert(const Key& key, const Value& value){
+    Position<Key, Value>*itr = this->root;
+    bool dret = true;
+    while(itr != nullptr){
+        if (itr->getKey() > key){
+            itr = itr->left();
+            dret = false;
+        }
+        if (itr->getKey() < key){
+            itr = itr->right();
+            dret = true;
+        }
+    }
+    Position<Key, Value> nou = new Position<Key, Value>(key);
+    nou->addValue(value);
+    nou->setParent(itr);
+    nou->setLeft(nullptr);
+    nou->setRight(nullptr);
+    if(dret){
+        itr->setRight(nou);
+    } else{
+        itr->setLeft(nou);
+    }
+    return itr;
+}
+
+// Operadors
+template <class Key, class Value> void BinaryTree<Key, Value>::printPreOrder(const Position<Key, Value> *node = nullptr) const{
+
+}
+
+template <class Key, class Value> void BinaryTree<Key, Value>::printPostOrder(const Position<Key, Value> *node = nullptr) const{
+
+}
+
+template <class Key, class Value> bool BinaryTree<Key, Value>::identicalTree(const BinaryTree<Key, Value>& other) const{
+    
+}
+    
 #endif // BINARYTREE_H
--- Practica 3/Position.h
+++ Practica 3/Position.h
@@ -1,6 +1,13 @@
 #ifndef POSITION_H
 #define POSITION_H
 
+#include "Position.h"
+#include <vector> 
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
 template <class Key, class Value>
 class Position {
 public:
@@ -35,4 +42,107 @@ private:
     Position<Key, Value> esq;
     Position<Key, Value> dret;
 };
+
+
+template <class Key, class Value> Position<Key, Value>::Position(const Key clau){
+    this->key = clau;
+    this->pare = nullptr;
+    this->esq = nullptr;
+    this->dret = nullptr;
+}
+
+template <class Key, class Value> Position<Key, Value>::Position(const Position<Key, Value>& orig){
+    this->values = orig.getValues();
+    this->key = orig.getKey();
+    this->pare = nullptr;
+    this->esq = nullptr;
+    this->dret = nullptr;
+}
+
+template <class Key, class Value> Position<Key, Value>::~Position(){
+    if(this->esq != nullptr){
+        delete this->esq;
+    }
+    if(this->dret != nullptr){
+        delete this->dret;
+    }
+}
+
+// Modificadors
+template <class Key, class Value> void Position<Key, Value>::setParent(Position<Key, Value>* pos){
+    this-> pare = pos;
+}
+
+template <class Key, class Value> void Position<Key, Value>::setLeft(Position<Key, Value>* pos){
+    this-> esq = pos;
+}
+
+template <class Key, class Value> void Position<Key, Value>::setRight(Position<Key, Value>* pos){
+    this->der = pos;
+}
+
+// Consultors
+template <class Key, class Value> const Key& Position<Key, Value>::getKey() const{
+    return this->key;
+}
+
+template <class Key, class Value> const vector<Value>& Position<Key, Value>::getValues() const{
+    return this->values;
+}
+
+template <class Key, class Value> Position<Key, Value> Position<Key, Value>::parent() const{
+    return this->pare;
+}
+
+template <class Key, class Value> Position<Key, Value> Position<Key, Value>::left() const{
+    return this->esq;
+}
+
+template <class Key, class Value> Position<Key, Value> Position<Key, Value>::right() const{
+    return this->dret;
+}
+
+// Operacions
+template <class Key, class Value> bool Position<Key, Value>::isRoot() const{
+    return this->pare == nullptr;
+}
+
+template <class Key, class Value> bool Position<Key, Value>::isLeaf() const{
+    return this->esq == nullptr && this->der == nullptr;
+}
+
+template <class Key, class Value> int Position<Key, Value>::depth() const{
+    if (this->isRoot()){
+        return 0;
+    }
+    return this->pare->depth()+1;
+}
+
+template <class Key, class Value> int Position<Key, Value>::height() const{
+    if(this->isLeaf()){
+        return 1;
+    }
+    int num = 0;
+    int num2 = 0;
+       if (this->esq != nullptr){
+        num = this->esq->height()+1;
+    }
+    if (this->dret != nullptr){
+        num2 = this->dreta->height()+1;
+    }
+    if (num > num2){
+        return num;
+    } else{
+        return num2;
+    }
+}
+
+template <class Key, class Value> void Position<Key, Value>::addValue(const Value& value){
+    (this->values).push_back(value);
+}
+
+template <class Key, class Value> bool Position<Key, Value>::operator==(const Position<Key, Value>& other) const{
+    return other.getKey() == this->key;
+}
+
 #endif //POSITION_H
